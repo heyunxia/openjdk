9638bbf396af8e26bf11a678fba1f3b1cdddd1fa
mr <devnull@localhost>
2009-05-29 12:47:26 -0700

    Temporary hack to keep the legacy launcher working in modular mode

--- src/share/vm/runtime/os.cpp
+++ src/share/vm/runtime/os.cpp
@@ -899,6 +899,14 @@ bool os::set_boot_path(char fileSep, char pathSep) {
         "%/lib/jsse.jar:"
         "%/lib/jce.jar:"
         "%/lib/charsets.jar:"
+
+        // ## TEMPORARY hack to keep the legacy launcher working when
+        // ## only the boot module is installed (cf. j.l.ClassLoader)
+        "%/lib/modules/jdk.boot/7-ea/classes:"
+        "%/lib/modules/jdk.boot/7-ea/classes.jar:"
+        "%/lib/modules/jdk.boot/7-ea/resources:"
+        "%/lib/modules/jdk.boot/7-ea/resources.jar:"
+
         "%/classes";
     char* sysclasspath = format_boot_path(classpath_format, home, home_len, fileSep, pathSep);
     if (sysclasspath == NULL) return false;
