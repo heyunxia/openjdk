5db1518b4c793ace4fbf11b70641f2b5f67af7d7
mchung <devnull@localhost>
2012-01-25 09:05:35 -0800

    Module views support: jdk.boot is replaced with jdk.base

--- src/share/vm/runtime/os.cpp
+++ src/share/vm/runtime/os.cpp
@@ -1141,10 +1141,7 @@ bool os::set_boot_path(char fileSep, char pathSep) {
 
         // ## TEMPORARY hack to keep the legacy launcher working when
         // ## only the boot module is installed (cf. j.l.ClassLoader)
-        "%/lib/modules/jdk.boot/8-ea/classes:"
-        "%/lib/modules/jdk.boot/8-ea/classes.jar:"
-        "%/lib/modules/jdk.boot/8-ea/resources:"
-        "%/lib/modules/jdk.boot/8-ea/resources.jar:"
+        "%/lib/modules/jdk.base/8-ea/classes:"
         "%/classes";
     char* sysclasspath = format_boot_path(classpath_format, home, home_len, fileSep, pathSep);
     if (sysclasspath == NULL) return false;
